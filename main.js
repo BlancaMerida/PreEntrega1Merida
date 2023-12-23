7a4968a2ef12624bb11ecbfb11e90491fc1740d7
Blanca Merida <blancamerida15@gmail.com>
2023-12-23 01:28:26 -0300

    segundo

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function calculadora(numeroUno, numeroDos, operacion){
+// CALCULADORA
+let seguir = "si";
+function calculadora(numeroUno, numeroDos, operacion) {
     switch (operacion) {
         case "+":
             return numeroUno + numeroDos;
@@ -15,13 +17,13 @@ function calculadora(numeroUno, numeroDos, operacion){
         case "/":
             return numeroUno / numeroDos;
             break;
-            
+
         default:
             return "Operacion no valida";
             break;
     }
 }
-
+while(seguir.toLowerCase()!="no"){
 let numeroUno = Number(prompt("Ingrese un numero"));
 let numeroDos = Number(prompt("Ingrese el siguiente numero"));
 let operacion = prompt("Ingrese la operacion");
@@ -29,3 +31,36 @@ let operacion = prompt("Ingrese la operacion");
 let resultado = calculadora(numeroUno, numeroDos, operacion);
 
 alert(`${numeroUno} ${operacion} ${numeroDos} = ${resultado}`);
+seguir = prompt(`desea seguir? (si/ no)`)
+}
+// OTRO CALCULO PRACTICANDO
+
+
+ //   let producto = prompt("¿que va a comprar?")
+ //   if(producto === "salchipapa"){
+//        alert("El plato de sachipapa vale 1000 ")
+//    }
+//    else if(producto === "jugodelimon"){
+//        alert("el jugo de limon tiene el valor de 800")
+//    }
+//    else if(producto ==="pancho"){
+//    alert("el pancho tiene el valor de 550")
+//    }
+//    else{
+//        alert("ese producto no esta ala venta")
+//    }
+//    let dineroAPagar = Number(prompt("concuanto dinero desa pagar?"))
+//    if(producto === "salchipapa"){
+//        alert("tu vuelto es:" +" " + "$" +(dineroAPagar - 1000))
+//    }
+//    else if(producto === "jugodelimon"){
+//        alert("tu vuelto es:" +" " + "$" +(dineroAPagar - 800))
+//    }
+//    else if(producto === "pancho"){
+//        alert("tu vuelto es:" +" " + "$" +(dineroAPagar - 550))
+//    }
+//    else{
+//        alert("nose pudo completar la compra")
+//    }
+
+
