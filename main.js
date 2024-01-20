32c730f6d211aae2f893b54f8b44bf8cc820ca59
Blanca Merida <blancamerida15@gmail.com>
2024-01-20 02:07:56 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,66 +1,69 @@
 // CALCULADORA
-let seguir = "si";
-function calculadora(numeroUno, numeroDos, operacion) {
-    switch (operacion) {
-        case "+":
-            return numeroUno + numeroDos;
-            break;
+//let seguir = "si";
+//function calculadora(numeroUno, numeroDos, operacion) {
+//    switch (operacion) {
+//        case "+":
+//            return numeroUno + numeroDos;
+//            break;
+//
+//        case "-":
+//            return numeroUno - numeroDos;
+//            break;
+//
+//        case "*":
+//            return numeroUno * numeroDos;
+//            break;
+//
+//        case "/":
+//            return numeroUno / numeroDos;
+//            break;
+//
+//        default:
+//            return "Operacion no valida";
+//            break;
+//    }
+//}
+//while(seguir.toLowerCase()!="no"){
+//let numeroUno = Number(prompt("Ingrese un numero"));
+//let numeroDos = Number(prompt("Ingrese el siguiente numero"));
+//let operacion = prompt("Ingrese la operacion");
+//
+//let resultado = calculadora(numeroUno, numeroDos, operacion);
+//
+//alert(`${numeroUno} ${operacion} ${numeroDos} = ${resultado}`);
+//seguir = prompt(`desea seguir? (si/ no)`)
+//}
 
-        case "-":
-            return numeroUno - numeroDos;
-            break;
+//            SEGUNDA ENTREGA
 
-        case "*":
-            return numeroUno * numeroDos;
-            break;
+const productos =  [
+    { id: 1, nombre: "cuaderno", precio: 1500 },
+    { id: 2, nombre: "lapicera", precio: 850 },
+    { id: 3, nombre: "carpeta", precio: 1900},
+    { id: 4, nombre: "resaltador", precio: 1000 },
+    { id: 5, nombre: "libros", precio: 900 },
+];
+let nombre = prompt("Ingrese el nombre del producto a buscar");
+const producto = productos.find((item) => item.nombre === nombre);
+if (producto) { 
+    alert(` 
+    Id: ${producto.id}
+    Nombre: ${producto.nombre}
+    Precio:${producto.precio}
+    `);
+} else {
+    alert("Producto no disponible");
 
-        case "/":
-            return numeroUno / numeroDos;
-            break;
-
-        default:
-            return "Operacion no valida";
-            break;
-    }
 }
-while(seguir.toLowerCase()!="no"){
-let numeroUno = Number(prompt("Ingrese un numero"));
-let numeroDos = Number(prompt("Ingrese el siguiente numero"));
-let operacion = prompt("Ingrese la operacion");
 
-let resultado = calculadora(numeroUno, numeroDos, operacion);
+const precio = Number(prompt("Ingrese el precio minimo del producto"));
+const filtra = productos.filter((item) => item.precio > precio);
 
-alert(`${numeroUno} ${operacion} ${numeroDos} = ${resultado}`);
-seguir = prompt(`desea seguir? (si/ no)`)
-}
-// OTRO CALCULO PRACTICANDO
-
-
- //   let producto = prompt("¿que va a comprar?")
- //   if(producto === "salchipapa"){
-//        alert("El plato de sachipapa vale 1000 ")
-//    }
-//    else if(producto === "jugodelimon"){
-//        alert("el jugo de limon tiene el valor de 800")
-//    }
-//    else if(producto ==="pancho"){
-//    alert("el pancho tiene el valor de 550")
-//    }
-//    else{
-//        alert("ese producto no esta ala venta")
-//    }
-//    let dineroAPagar = Number(prompt("concuanto dinero desa pagar?"))
-//    if(producto === "salchipapa"){
-//        alert("tu vuelto es:" +" " + "$" +(dineroAPagar - 1000))
-//    }
-//    else if(producto === "jugodelimon"){
-//        alert("tu vuelto es:" +" " + "$" +(dineroAPagar - 800))
-//    }
-//    else if(producto === "pancho"){
-//        alert("tu vuelto es:" +" " + "$" +(dineroAPagar - 550))
-//    }
-//    else{
-//        alert("nose pudo completar la compra")
-//    }
-
+filtra.forEach((item) => {
+    alert(`
+    Id: ${item.id}
+    Nombre: ${item.nombre}
+    Precio:${item.precio}
+    `);
+});
 
